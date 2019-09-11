8d4e7e8ff331203925e612c2a539cdca9c60fa5a
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-09-11 18:59:47 +0530

    added setup_libreoffice

---
+++ setup_libreoffice.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# install libreoffice
+sudo apt install libreoffice-core libreoffice-common libreoffice-gnome libreoffice-gtk3 -y
+sudo apt install libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-base -y
+
