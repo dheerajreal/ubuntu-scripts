4ed8c06f2e37f2e91cfef93eae91c343c2b5bfbc
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-11-24 20:09:08 +0530

    improved formatting

--- qtfix.sh
+++ qtfix.sh
@@ -3,5 +3,4 @@
 #this script will fix that
 #a reboot might be needed
 
-
 sudo sed -i -e '1iexport QT_AUTO_SCREEN_SCALE_FACTOR=0\' /etc/environment
--- setup_jekyll.sh
+++ setup_jekyll.sh
@@ -7,13 +7,12 @@
 
 sudo apt install ruby-full build-essential zlib1g-dev
 
-#It is best to avoid installing Ruby Gems as the root user. 
+#It is best to avoid installing Ruby Gems as the root user.
 #Therefore, we need to set up a gem installation directory for your user account.
 
-
-echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
-echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
-echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+echo '# Install Ruby Gems to ~/gems' >>~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >>~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >>~/.bashrc
 source ~/.bashrc
 
 #Finally, install Jekyll
@@ -23,4 +22,3 @@ echo "gem install jekyll bundler" | bash
 #That’s it! You’re ready to start using Jekyll.
 
 echo "done"
-
