61b0a508c56d33338618fefe36abe3d4b14dafb9
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2020-07-30 18:06:38 +0530

    added snapswap

---
+++ setup/snapswap.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo snap remove gnome-calculator gnome-characters gnome-logs gnome-system-monitor
+sudo apt install gnome-calculator gnome-characters gnome-logs gnome-system-monitor -y
