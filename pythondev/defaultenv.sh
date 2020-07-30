4d6f54dbb950f6c1d576fa9404ccead95979b846
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2020-07-30 19:59:33 +0530

    pythondev

--- pythondev/defaultenv.sh
+++ pythondev/defaultenv.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd ~
 python3 -m venv .defaultpyenv
-echo "alias venv='source ~/.defaultpyenv/bin/activate' " >>~/.bashrc
+
+echo "# default python virtualenvironment " >>~/.bashrc
+echo "alias defaultpyenv='source ~/.defaultpyenv/bin/activate' " >>~/.bashrc
 
 echo "activate by typing:  source .defaultpyenv/bin/activate"
 echo "or reload shell and type defaultpyenv"
