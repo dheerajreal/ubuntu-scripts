d612b18d7b34d8fe4995b6ccbeeb9dec6c4074e8
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-04-22 20:06:14 +0530

    improved hostsfile and docker setup

--- setup/docker.sh
+++ setup/docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt install docker.io -y
-sudo docker run hello-world
+sudo apt install docker.io docker-compose -y
+# sudo docker run hello-world
 sudo groupadd docker
 sudo usermod -aG docker $USER
 echo "done"
--- setup/hostsfile.sh
+++ setup/hostsfile.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+sudo echo "Downloading hosts file "
 cd /tmp
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -O hosts
 
 if [ -f /etc/hostsbkup ]; then
     echo "backup found , skip creating backup"
-    sudo rm /etc/hosts
+    # sudo rm /etc/hosts
 else
     echo "backup not found , create backup"
     sudo mv /etc/hosts /etc/hostsbkup
 fi
 echo "Installing hosts file"
-sudo cp ./hosts /etc/hosts
+echo "0.0.0.0 example.com" >> /tmp/hosts
+sudo cp /tmp/hosts /etc/hosts
 echo "Installation complete"
